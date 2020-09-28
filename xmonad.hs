ac7c0721ef8c1f7f1a964902fa8444572dd9fac6
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-09-28 16:51:50 +0200

    Update dotfiles

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,11 @@
+import Control.Monad (forM, forM_)
+
 import XMonad
 -- import XMonad.Hooks.DynamicLog (dynamicLogWithPP, xmobarPP)
 import qualified XMonad.Hooks.DynamicLog as Log
 import qualified XMonad.Hooks.ManageDocks as Docks
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Util.EZConfig (additionalKeys)
 import qualified XMonad.Layout.IndependentScreens as LIS
 import qualified Graphics.X11.ExtraTypes.XF86 as XF86
@@ -10,16 +13,45 @@ import qualified XMonad.Actions.NoBorders as NoBorders
 
 import System.IO (hPutStrLn)
 
+
+myStartupHook :: Int -> X ()
+myStartupHook screenNum = do
+  -- spawnOnce "nitrogen --restore &"
+  -- spawnOnce "picom &"
+  spawnOnce "nm-applet &"
+  spawnOnce "udiskie --tray &"
+  spawnOnce "feh --bg-scale /usr/share/backgrounds/gnome/Icescape.jpg &"
+  spawnOnce "volumeicon &"
+  -- Do not use the main (laptop) monitor in case there's another one available
+  spawnOnce $ unwords
+    [ "trayer --edge top --align right --height 17 --width 10 --SetDockType true --SetPartialStrut true --expand true"
+    , "--monitor", show $ if screenNum > 1 then 1 else 0 :: Int
+    , "--transparent true --alpha 0 --tint 0x0 &"
+    ]
+    -- --padding 6 --widthtype request
+  -- spawnOnce "/usr/bin/emacs --daemon &"
+  -- -- spawnOnce "kak -d -s mysession &"
+  -- setWMName "LG3D"
+
+
+main :: IO ()
 main = do
-  xmproc <- spawnPipe "xmobar"
+  -- Spawn one xmobar session per each screen
+  screenNum <- LIS.countScreens
+  xmprocs <- forM [0 .. screenNum-1 :: Int] $ \k -> do
+    spawnPipe $ "xmobar -x " ++ show k
+
   xmonad $ Docks.docks def
-    { manageHook = Docks.manageDocks <+> manageHook defaultConfig
-    , layoutHook = Docks.avoidStruts $ layoutHook defaultConfig
+    { manageHook = Docks.manageDocks <+> manageHook def
     -- , layoutHook = Docks.avoidStruts $ myLayout
-    , logHook = Log.dynamicLogWithPP Log.xmobarPP
-        { Log.ppOutput = hPutStrLn xmproc
-        , Log.ppTitle = Log.xmobarColor "green" "" . Log.shorten 50
-        }
+    , layoutHook = Docks.avoidStruts $ layoutHook def
+    -- Handle each xmobar session individually
+    , logHook = forM_ xmprocs $ \xmproc -> do
+        Log.dynamicLogWithPP Log.xmobarPP
+          { Log.ppOutput = hPutStrLn xmproc
+          , Log.ppTitle = Log.xmobarColor "green" "" . Log.shorten 50
+          }
+    , startupHook = myStartupHook screenNum
     , modMask = mod4Mask     -- Rebind Mod to the Windows key
     }
     `additionalKeys`
@@ -27,10 +59,11 @@ main = do
     -- function keys
       ((0, XF86.xF86XK_MonBrightnessUp), spawn "sudo sysbacklight up")
     , ((0, XF86.xF86XK_MonBrightnessDown), spawn "sudo sysbacklight down")
-    , ((0, XF86.xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
-    , ((0, XF86.xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
-    -- , ((0, XF86.xF86XK_AudioMute), spawn "amixer set Master toggle")
-    , ((0, XF86.xF86XK_AudioMute), spawn "amixer -D pulse set Master 1+ toggle")
+
+    , ((0, XF86.xF86XK_AudioLowerVolume), spawn "amixer set Master 1%-")
+    , ((0, XF86.xF86XK_AudioRaiseVolume), spawn "amixer set Master 1%+")
+    , ((0, XF86.xF86XK_AudioMute), spawn "amixer set Master toggle")
+    -- , ((0, XF86.xF86XK_AudioMute), spawn "amixer -D pulse set Master 1+ toggle")
 
     , ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
     , ((mod4Mask .|. shiftMask, xK_s), spawn "systemctl suspend")
